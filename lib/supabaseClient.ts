c0d448a250340f8a41c2741fbf680f94f8a9ed3c
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-03 00:35:41 +0800

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,3 +1,6 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from './types';
-export const supabase = createClientComponentClient<Database>();
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
