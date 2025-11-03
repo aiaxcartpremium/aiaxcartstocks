ffc86ac68ce2f8a821ffb21585fbf860f7733b76
aiaxcartpremium <shanaiamau99@gmail.com>
2025-11-03 16:10:14 +0800

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,6 @@
-// /lib/supabaseClient.ts
-'use client';
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  { auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true } }
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
